399dd7e2acc747cfd6a7a4409141b3504088a86d
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-05-15 09:30:02 +0200

    Minimal tests

--- tests/test_apt_local.py
+++ tests/test_apt_local.py
@@ -5,18 +5,38 @@
 
 
 import unittest
+from contextlib import suppress, redirect_stdout
+import os
+from apt_local import init_parser
 
-from apt_local import main
 
-
-class TestApt_local(unittest.TestCase):
+class TestAptLocal(unittest.TestCase):
     """Tests for `apt_local` package."""
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         """Set up test fixtures, if any."""
+        cls.parser = init_parser()
+        cls.devnull = open(os.devnull, "w")
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         """Tear down test fixtures, if any."""
+        cls.devnull.close()
 
-    def test_000_something(self):
+    def test_help(self):
         """Test something."""
+        print("apt-local", "-h")
+        with suppress(SystemExit), redirect_stdout(self.devnull):
+            args = self.parser.parse_args(["-h"])
+        for cmd in (
+            "install",
+            "uninstall",
+            "list",
+            "search",
+            "set-path",
+            "show-path",
+        ):
+            print("apt-local", cmd, "-h")
+            with suppress(SystemExit), redirect_stdout(self.devnull):
+                args = self.parser.parse_args([cmd, "-h"])
